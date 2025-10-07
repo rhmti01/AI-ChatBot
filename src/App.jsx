e808eb56ddeceb1a21fbeb0cb85379979e0eed75
rhmti01 <rahmati.business@gmail.com>
2025-10-07 23:10:27 +0330

    add starter structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <p className=" p-20 text-center text-2xl font-medium " >Ai ChatBot React</p>
+    </>
+  );
+}
+
+export default App;
